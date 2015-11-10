adf6e3e4dc1f67467fbad63ca2d47f954f6cd451
mainnmotion <kevin@maninmotion.com>
2015-11-10 10:18:49 -0500

    Initial management scripts

--- LittleSportsBiscuit/management/commands/getDeep.py
+++ LittleSportsBiscuit/management/commands/getDeep.py
@@ -1,9 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 from LittleSportsBiscuit.models import Team, Player, Roster
 
 class Command(BaseCommand):
     help = "This is the help line"
 
     def handle(self, *args, **options):
-        #http://www.ourlads.com/ncaa-football-depth-charts/depth-chart/south-carolina/91832
+        thisUrl = "http://www.ourlads.com/ncaa-football-depth-charts/depth-chart/south-carolina/91832"
+        thisCode = urlopen(thisUrl)
+        thisObj = BeautifulSoup(thisCode, "html.parser")
+        print(thisObj.title)
         self.stdout.write("There are {} many Teams".format(Team.objects.count()))
+        #self.stdout.write(thisObj.title)
