e09f85da1d34dd065add7fbea663748b1be80218
mainnmotion <kevin@maninmotion.com>
2015-11-10 08:33:38 -0500

    Initial management scripts

--- LittleSportsBiscuit/management/commands/getDeep.py
+++ LittleSportsBiscuit/management/commands/getDeep.py
@@ -5,4 +5,5 @@ class Command(BaseCommand):
     help = "This is the help line"
 
     def handle(self, *args, **options):
+        #http://www.ourlads.com/ncaa-football-depth-charts/depth-chart/south-carolina/91832
         self.stdout.write("There are {} many Teams".format(Team.objects.count()))
